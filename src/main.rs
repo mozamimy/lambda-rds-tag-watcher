44b12250eed68763c7e8629180c8c3d9eea61396
mozamimy <alice@mozami.me>
2018-12-03 22:26:53 +0900

    Do not send message when there are no target instances

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 extern crate aws_lambda as lambda;
+extern crate env_logger;
 #[macro_use]
 extern crate failure;
+#[macro_use]
+extern crate log;
 extern crate rusoto_core;
 extern crate rusoto_rds;
 extern crate rusoto_sns;
@@ -9,6 +12,8 @@ use rusoto_rds::Rds;
 use rusoto_sns::Sns;
 
 fn main() {
+    let _ = env_logger::try_init();
+
     lambda::start(|_: lambda::event::cloudwatch_events::CloudWatchEvent| {
         let rds = rusoto_rds::RdsClient::new(rusoto_core::Region::default());
 
@@ -53,15 +58,20 @@ fn main() {
             }
         }
 
-        let sns = rusoto_sns::SnsClient::new(rusoto_core::Region::default());
-        let mut publish_input: rusoto_sns::PublishInput = Default::default();
-        publish_input.topic_arn = Some(std::env::var("SNS_TOPIC_ARN")?);
-        publish_input.subject = Some(std::env::var("SNS_SUBJECT").unwrap_or(format!("*Attach {} tag please!*", tag)));
-        publish_input.message = db_instance_arns.join("\n");
+        if db_instance_arns.len() == 0 {
+            info!("There are no instances to notify.");
+            return Ok("ok")
+        } else {
+            let sns = rusoto_sns::SnsClient::new(rusoto_core::Region::default());
+            let mut publish_input: rusoto_sns::PublishInput = Default::default();
+            publish_input.topic_arn = Some(std::env::var("SNS_TOPIC_ARN")?);
+            publish_input.subject = Some(std::env::var("SNS_SUBJECT").unwrap_or(format!("*Attach {} tag please!*", tag)));
+            publish_input.message = db_instance_arns.join("\n");
 
-        match sns.publish(publish_input).sync() {
-            Err(e) => return Err(format_err!("{:?}", e)),
-            Ok(_) => return Ok("ok"),
+            match sns.publish(publish_input).sync() {
+                Err(e) => return Err(format_err!("{:?}", e)),
+                Ok(_) => return Ok("ok"),
+            }
         }
     })
 }
